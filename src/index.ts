713fbd08e191a73ba2632d713c4814b9fd969312
a-morphous <84998015+a-morphous@users.noreply.github.com>
2026-01-11 15:46:10 -0800

    Expose SerializedEntity type

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { Component } from './Component';
 export type { EntityType as Entity } from './Entity';
 export type { Query, QueryFilter } from './Query';
 export type { World } from './World';
+export type { SerializedEntity } from "./types/basic-types"
